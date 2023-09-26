ff6370b8cc16ac3ca7f19d66afa1e114d1fc6ee8
Dark045 <87452921+Dark045NL@users.noreply.github.com>
2023-09-26 14:29:18 +0200

    First Commit

---
+++ app/src/main/java/com/example/doner_map/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.doner_map
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            // Define the action to perform when the button is clicked.
+            // For example, you can start another activity.
+            val intent = Intent(this, MainActivityMap::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/doner_map/MainActivityMap.kt
@@ -0,0 +1,11 @@
+package com.example.doner_map
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityMap : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_map)
+    }
+}
