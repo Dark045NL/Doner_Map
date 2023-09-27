ee1909845363f6b305024f0e1edcfc76857c62fc
Ian Huis in 't veld <ianh241203@gmail.com>
2023-09-27 13:33:35 +0200

    Added Google maps

--- app/src/main/java/com/example/doner_map/MainActivityMap.kt
+++ app/src/main/java/com/example/doner_map/MainActivityMap.kt
@@ -1,11 +1,25 @@
 package com.example.doner_map
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 
 class MainActivityMap : AppCompatActivity() {
+
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_map)
+
+        val webView:WebView=findViewById(R.id.webView)
+        webView.webViewClient= WebViewClient()
+
+        val url="https://www.google.com/maps/search/d%C3%B6ner"
+        webView.loadUrl(url)
+
+        val webSettings=webView.settings
+        webSettings.javaScriptEnabled=true
     }
 }
