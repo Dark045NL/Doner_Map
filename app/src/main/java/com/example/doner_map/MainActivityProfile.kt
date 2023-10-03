4018b485c865848560b43114dce7dc2f3891278e
7Kice <77838631+7Kice@users.noreply.github.com>
2023-10-03 13:39:48 +0200

    Login

---
+++ app/src/main/java/com/example/doner_map/MainActivityProfile.kt
@@ -0,0 +1,39 @@
+package com.example.doner_map
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+
+class MainActivityProfile : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_profile)
+
+        auth = FirebaseAuth.getInstance()
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        val googleSignInClient = GoogleSignIn.getClient(this , gso)
+
+        val email = intent.getStringExtra("email")
+        val displayName = intent.getStringExtra("name")
+
+        findViewById<TextView>(R.id.textView).text = email + "\n" + displayName
+
+        findViewById<Button>(R.id.SIgnOutBtn).setOnClickListener{
+            googleSignInClient.signOut()
+            startActivity(Intent(this , MainActivity::class.java))
+        }
+    }
+}
