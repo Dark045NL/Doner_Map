01c06608082b69eab84e648594f53c033e7dd3d8
Dark045 <87452921+Dark045NL@users.noreply.github.com>
2023-10-10 12:18:39 +0200

    Update Manifest

--- app/src/main/java/com/example/doner_map/MainActivityProfile.kt
+++ app/src/main/java/com/example/doner_map/MainActivityProfile.kt
@@ -3,10 +3,15 @@ package com.example.doner_map
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.firestore.FirebaseFirestore
@@ -14,10 +19,37 @@ import com.google.firebase.firestore.FirebaseFirestore
 class MainActivityProfile : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
+    lateinit var toggle : ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_profile)
+
+        val drawerLayout : DrawerLayout = findViewById(R.id.drawer_layout)
+        val navView : NavigationView = findViewById(R.id.nav_view)
+
+        toggle = ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navView.setNavigationItemSelectedListener {
+
+            when(it.itemId){
+
+                R.id.nav_home -> { val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)}
+                R.id.nav_map -> { val intent = Intent(this, MainActivityMap::class.java)
+                    startActivity(intent) }
+                R.id.nav_profile -> { val intent = Intent(this, MainActivityLogin::class.java)
+                    startActivity(intent) }
+                R.id.nav_ratings -> Toast.makeText(applicationContext, "Clicked Ratings", Toast.LENGTH_SHORT).show()
+                R.id.nav_settings -> Toast.makeText(applicationContext, "Clicked Settings", Toast.LENGTH_SHORT).show()
+
+            }
+            true
+        }
 
         val user = FirebaseAuth.getInstance().currentUser
         val userID = user?.uid
@@ -43,5 +75,11 @@ class MainActivityProfile : AppCompatActivity() {
             googleSignInClient.signOut()
             startActivity(Intent(this , MainActivityLogin::class.java))
         }
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
