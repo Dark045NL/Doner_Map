548984243f683a7b9d69353c28d609f78cc33f09
7Kice <77838631+7Kice@users.noreply.github.com>
2023-10-10 12:07:29 +0200

    Instance 3

--- app/src/main/java/com/example/doner_map/MainActivityProfile.kt
+++ app/src/main/java/com/example/doner_map/MainActivityProfile.kt
@@ -22,9 +22,10 @@ class MainActivityProfile : AppCompatActivity() {
         val user = FirebaseAuth.getInstance().currentUser
         val userID = user?.uid
 
-        val database = FirebaseDatabase.getInstance()
-        val usersRef = database.getReference("users")
+        val database = FirebaseDatabase.getInstance("https://donermap-default-rtdb.europe-west1.firebasedatabase.app/")
+        val usersRef = database.getReference()
 
+        usersRef.setValue(userID)
         auth = FirebaseAuth.getInstance()
 
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
